20998fa0b971f67d370e265bdbc8da384c1e8232
mduernberger <melanie.duernberger05@htl-salzburg.ac.at>
2022-03-25 18:58:29 +0100

    script updated

--- Assets/MyGame/Scripts/HeyHey.cs
+++ Assets/MyGame/Scripts/HeyHey.cs
@@ -7,7 +7,7 @@ public class HeyHey : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Gustav");
     }
 
     // Update is called once per frame
